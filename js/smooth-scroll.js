26dc2f5b03899ad9d0d1cee1825e7a1f8571574d
mustafakhalidcs <mustafakhalidcs@gmail.com>
2016-08-14 18:20:44 +0500

    initial commit

---
+++ js/smooth-scroll.js
@@ -0,0 +1,12 @@
+$('.navbar-default .nav li a').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top -100
+            }, 1000);
+            return false;
+        }
+    }
+});
