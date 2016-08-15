21ec6d6a05fbd5d312414fd2f6cd353753e1c235
mustafakhalidcs <mustafakhalidcs@gmail.com>
2016-08-15 11:48:58 +0500

    Latest code

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -4,7 +4,7 @@ $('.navbar-default .nav li a').click(function() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
             $('html,body').animate({
-                scrollTop: target.offset().top -100
+                scrollTop: target.offset().top - 100
             }, 1000);
             return false;
         }
